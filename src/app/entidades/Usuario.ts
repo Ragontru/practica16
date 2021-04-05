f8512a6166480d54441355700fa7984687608c00
Raquel <ragontru25@gmail.com>
2021-04-05 11:12:47 +0200

    sin acabar

---
+++ src/app/entidades/Usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+
+    constructor(
+        public name: string,
+        public lastname: string,
+        public birthday: string,
+        public dni: string) { }
+}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavigationExtras } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Usuario } from '../entidades/Usuario';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,33 @@ import { NavController } from '@ionic/angular';
 export class HomePage implements OnInit {
 
   validations_form: FormGroup;
-  genders: Array<string>;
+  dni_validation: FormGroup;
+
+  validation_messages = {
+    'name': [
+      { type: 'required', message: 'Nombre requerido' },
+      { type: 'minlength', message: 'Mínimo debe contener, al menos 2, letras' },
+      { type: 'pattern', message: 'Sólo permite letras' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Apellido requerido' },
+      { type: 'minlength', message: 'Mínimo debe contener, al menos, 2 letras' },
+      { type: 'pattern', message: 'Sólo permite letras' }
+    ],
+    'birthday': [
+      { type: 'required', message: 'Fecha de nacimiento requerida' }
+    ],
+    'dni': [
+      { type: 'required', message: 'DNI requerido' },
+      { type: 'minlength', message: 'DNI debe contener 9 caracteres' },
+      { type: 'maxlength', message: 'DNI no debe contener más de  9 caracteres' },
+      { type: 'pattern', message: 'DNI debe estar formado por 8 números seguido de una letra' },
+      { type: 'validDni', message: 'El DNI introducido no es válido' }
+    ],
+    'usuario_form': [
+      { type: 'validarUsuario', message: 'Debe introducir el DNI al ser mayor de edad' }
+    ]
+  }
 
   constructor(
     public formBuilder: FormBuilder,
@@ -20,7 +47,7 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
 
-    this.validations_form = this.formBuilder.group({
+    this.validations_form = new FormGroup({
       name: new FormControl('', Validators.compose([
         Validators.minLength(2),
         Validators.pattern('^[A-Za-z ]*$'),
@@ -40,10 +67,21 @@ export class HomePage implements OnInit {
         Validators.required
       ]))
     });
+
+    this.dni_validation = new FormGroup({
+      
+    })
   }
 
   onSubmit(values) {
     console.log(values);
+
+    let usuario: Usuario;
+    usuario = new Usuario(values['usuario_form']['name'],
+      values['usuario_form']['lastname'],
+      values['usuario_form']['birthday'],
+      values['usuario_form']['dni']);
+
     let navigationExtras: NavigationExtras = {
       queryParams: {
         user: JSON.stringify(values),
@@ -69,5 +107,21 @@ export class HomePage implements OnInit {
 
   }
 
+  validarUsuario(fg: FormGroup) {
+
+    var fechaNacimiento: Date = fg.controls['dni'].value;
+    var fechaActual = Math.abs(Date.now()-fechaNacimiento.getTime());
+    var edad = Math.floor((fechaActual/(1000*3600*24))/365);
+
+    
+    console.log(edad);
+
+    if (edad > 18) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
 
 }
